5f0fc958f2a3a41543b862deef034e9e6b7b7446
Pablo Contreras <cont.pab@gmail.com>
2020-03-23 15:59:37 -0300

    inicio

---
+++ core/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, HttpResponse
+
+html_base = """
+<h1>Mi web personal</h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about/">Aserca de</a></li>
+    <li><a href="/portafolio/">Portafolio</a></li>
+    <li><a href="/contact/">Contacto</a></li>
+"""
+
+# Create your views here.
+def home(request):
+    return HttpResponse(html_base +"<h1>esta es el home</h1><h2>esta es el home</h2>")
+
+def about(request):
+    return HttpResponse(html_base +"<h1>Aceerca de</h1>")
+
+def portafolio(request):
+    return HttpResponse(html_base +"<h1>esta es el portafolio</h1>")
+
+def contact(request):
+    return HttpResponse(html_base +"<h1>esta es el contacto</h1>")
